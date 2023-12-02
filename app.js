b4032b6bcca9bcd5f67a64d567602dfba9afbde8
Avigithub8 <sharma.avi844@gmail.com>
2023-12-02 17:59:18 +0530

    use add product redirect

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-//const http = require('http');
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-const app=express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next()
-  })
+// app.use("/", (req, res, next) => {
+//   console.log("This always runs!");
+//   next();
+// });
 
-app.use((req,res,next)=>{
-  console.log('In another middleware');
-  res.send('<h1>Hello Express.js</h1>')
-})
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product></button></form>'
+  );
+});
 
-// const server = http.createServer(app);
-// server.listen(3000);
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
-app.listen(3000)
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello Express.js</h1>");
+});
+
+app.listen(3000);
