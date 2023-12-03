5b007cb7a0c78322dc9c47690120520731215ad4
Avigithub8 <sharma.avi844@gmail.com>
2023-12-04 00:33:57 +0530

    edit and delete fuunctionalies and convert chat-app to shopping-app name

--- shopping-app/controllers/shop.js
+++ shopping-app/controllers/shop.js
@@ -11,19 +11,16 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-
-exports.getProduct=(req,res,next)=>{
-  const prodId=req.params.productId;
-  console.log(prodId);
-  Product.findById(prodId,product=>{
-    res.render('shop/product-detail',{
-      product:product,
-      pageTitle:product.title,
-      path:"/products"
-    })
-  })
-  
-}
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -36,9 +33,23 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
   });
 };
 
@@ -48,6 +59,14 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
 };
 
 exports.getOrders = (req, res, next) => {
