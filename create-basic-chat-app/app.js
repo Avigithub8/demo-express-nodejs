610dd4109d0769b1b66deb32838752e51c3a4711
Avigithub8 <sharma.avi844@gmail.com>
2023-12-03 12:54:13 +0530

    some change and used html and ejs create-basic-chat-app  folder

--- create-basic-chat-app/app.js
+++ create-basic-chat-app/app.js
@@ -1,39 +1,29 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.get("/login", (req, res, next) => {
-  res.send(`
-    <form action="/message" method="GET">
-      <input type="text" name="username" placeholder="username"/>
-      <button type="submit">Login</button>
-    </form>
-  `);
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
 });
 
-app.get("/message", (req, res, next) => {
+app.get("/message", (req, res) => {
   const username = req.query.username;
   res.cookie("username", username);
   res.redirect("/");
 });
 
-app.get("/", (req, res, next) => {
-  const username = req.cookies.username || "Unknown User";
-  res.send(`<p>${username}: ${fs.readFileSync("message.txt", "utf8")} </p>
-    <form action="/send" method="POST">
-      <input type="text" name="message" />
-      <button type="submit">Send</button>
-    </form>
-     `);
-});
+app.get("/", (req, res) => {
+    const username = req.cookies.username || "Unknown User";
+    const messages = fs.readFileSync("message.txt", "utf8");
+    res.render("home.ejs", { username, messages });
+  });
 
 app.post("/send", (req, res) => {
   const username = req.cookies.username || "Unknown User";
@@ -44,4 +34,29 @@ app.post("/send", (req, res) => {
   res.redirect("/");
 });
 
+app.get("/contactus", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "contactus.html"));
+});
+
+app.post("/submit-contact", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+
+ 
+  console.log(`Contact Form Submission: Name - ${name}, Email - ${email}`);
+
+  res.redirect("/success");
+});
+
+app.get("/success", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "success.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "notfound.html"));
+});
+
 app.listen(3000);
+ 
+
+
