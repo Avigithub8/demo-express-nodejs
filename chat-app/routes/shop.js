4da8b5bb9721fed9de643f99a71e0b947e6dc7de
Avigithub8 <sharma.avi844@gmail.com>
2023-12-03 14:10:28 +0530

    chat-app folder make controller

---
+++ chat-app/routes/shop.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const productController = require('../controller/products');
+
+
+const router = express.Router();
+
+router.get('/', productController.getProducts );
+
+module.exports = router;
