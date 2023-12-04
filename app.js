4ba971773ad8092f1b2d8ede97bddad7b8cb6a88
Avigithub8 <sharma.avi844@gmail.com>
2023-12-04 14:01:21 +0530

    database connection

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+
+
 const app = express();
+
+
 
 const adminRoutes=require('./routes/admin')
 const shopRoutes=require('./routes/shop')
