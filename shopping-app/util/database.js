3f4148db1a869037c54a53834e3a3a5c9c16a594
Avigithub8 <sharma.avi844@gmail.com>
2023-12-04 16:01:51 +0530

    create database using sequelize

--- shopping-app/util/database.js
+++ shopping-app/util/database.js
@@ -12,7 +12,7 @@
 
 const Sequelize = require('sequelize');
 
-const sequelize=new Sequelize('node_complete','root','root',{
+const sequelize=new Sequelize('node_complete','root','*****',{
     dialect:'mysql',
     host:'localhost'
 });
