0252b5dc99bef3b4598f66454838c9752aa2e40b
Avigithub8 <sharma.avi844@gmail.com>
2023-12-04 15:23:09 +0530

    retrive products from db

--- shopping-app/app.js
+++ shopping-app/app.js
@@ -14,7 +14,11 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.end();
+db.execute('Select * FROM products').then((res)=>{
+  console.log(res[0],res[1]);
+}).catch((err)=>{
+   console.log(err)
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
